af780dfb676b06579b01b448643cdd2285aa948f
Jeffrey Smith II <jeffreyssmith2nd@gmail.com>
2019-10-31 16:18:54 -0400

    wip

--- tmp/complexsecondary/ncomplexsecondary.cpp
+++ tmp/complexsecondary/ncomplexsecondary.cpp
@@ -14,20 +14,34 @@ class [[eosio::contract]] ncomplexsingle : public contract {
       ncomplexsingle(name receiver, name code,  datastream<const char*> ds): contract(receiver, code, ds) {}
 
       [[eosio::action]]
-         void insert(uint64_t pk, name user) {
-            eosio::print_f("INSERT\n");
+         void insert(uint64_t pk, name user, uint64_t secondary) {
+            eosio::print_f("\n\nINSERT\n");
             address_index addresses( get_self(), get_first_receiver().value );
             auto itr = addresses.find(pk);
             check(itr == addresses.end(), "This record already exists");
             addresses.emplace(get_self(), [&]( auto& row ) {
                row.key = pk;
                row.user = user;
+               row.secondary = secondary;
             });
          }
 
       [[eosio::action]]
+         void get(uint64_t pk) {
+            eosio::print_f("\n\nGET\n");
+            address_index addresses( get_self(), get_first_receiver().value );
+
+            auto sec = addresses.get_index<name("secondary")>();
+
+            auto sitr = sec.find(9);
+
+            sitr = sec.lower_bound(9);
+            sitr = sec.upper_bound(8);
+         }
+
+      [[eosio::action]]
          void modify(uint64_t pk, name user) {
-            eosio::print_f("MODIFY\n");
+            eosio::print_f("\n\nMODIFY\n");
             address_index addresses( get_self(), get_first_receiver().value );
             auto itr = addresses.find(pk);
             check(itr != addresses.end(), "This record does not exist");
@@ -37,30 +51,42 @@ class [[eosio::contract]] ncomplexsingle : public contract {
          }
 
       [[eosio::action]]
-         void erase(uint64_t pk) {
-            eosio::print_f("ERASE\n");
-            address_index addresses( get_self(), get_first_receiver().value);
+         void move(uint64_t pk) {
+            eosio::print_f("\n\nMOVE\n");
+            address_index addresses( get_self(), get_first_receiver().value );
 
-            auto itr = addresses.find(pk);
-            check(itr != addresses.end(), "Record does not exist but should");
-            addresses.erase(itr);
-            itr = addresses.find(pk);
-            check(itr == addresses.end(), "Record exists when it shouldn't");
+            auto sec = addresses.get_index<name("secondary")>();
+
+            auto sitr = sec.find(9);
+            auto send = sec.end();
+            eosio::print_f("sitr %\n", sitr->key);
+
+            sitr--;
+            eosio::print_f("-- %\n", sitr->key);
+            sitr++;
+            eosio::print_f("++ %\n", sitr->key);
+            sitr++;
+            eosio::print_f("++ %\n", sitr->key);
+            sitr--;
+            eosio::print_f("-- %\n", sitr->key);
+            sitr++;
+            eosio::print_f("++ %\n", sitr->key);
          }
 
       [[eosio::action]]
-         void move(uint64_t pk) {
-            eosio::print_f("MOVE\n");
+         void erase(uint64_t pk) {
+            eosio::print_f("\n\nERASE\n");
             address_index addresses( get_self(), get_first_receiver().value);
+            auto sec = addresses.get_index<name("secondary")>();
+            auto sitr = sec.find(9);
 
-            auto itr = addresses.begin();
-            itr++;
-            itr++;
-            itr--;
-            itr--;
-            itr--;
+            check(sitr != sec.end(), "Record does not exist but should");
+            sec.erase(sitr);
+            sitr = sec.find(9);
+            check(sitr == sec.end(), "Record exists when it shouldn't");
          }
 
+#if 0
       [[eosio::action]]
          void move2(uint64_t pk) {
             eosio::print_f("MOVE2\n");
@@ -90,25 +116,31 @@ class [[eosio::contract]] ncomplexsingle : public contract {
             itr++;
             addresses.erase(itr);
          }
+#endif
 
    private:
       struct [[eosio::table]] person {
          uint64_t key;
          name user;
+         uint64_t secondary;
          uint64_t primary_key() const { return key; }
+         uint64_t by_secondary() const { return secondary; }
       };
-      typedef eosio::multi_index<"people"_n, person> address_index;
+      typedef eosio::multi_index<"people"_n, person, indexed_by<
+         name("secondary"), const_mem_fun<person, uint64_t, &person::by_secondary>
+      >> address_index;
 
 };
 
-EOSIO_DISPATCH(ncomplexsingle, (insert)(modify)(erase)(erase2)(erase3)(move)(move2))
+EOSIO_DISPATCH(ncomplexsingle, (insert)(get)(modify)(move)(erase))
 
 EOSIO_TEST_BEGIN(ncomplexsingle_test)
    intrinsics::set_intrinsic<intrinsics::read_action_data>(
       [](void* m, uint32_t len) {
-         check(len <= 2*sizeof(eosio::name), "failed from read_action_data");
+         check(len <= 3*sizeof(eosio::name), "failed from read_action_data");
          uint64_t pk = primary_key;
          uint64_t user;
+         uint64_t secondary = (-primary_key) + 10;
 
          switch (testnum) {
          case 0:
@@ -127,13 +159,14 @@ EOSIO_TEST_BEGIN(ncomplexsingle_test)
 
          memcpy(m, &pk, sizeof(uint64_t));
          memcpy((char*)m + sizeof(eosio::name), &user, sizeof(eosio::name));
+         memcpy((char*)m + 2*sizeof(uint64_t), &secondary, sizeof(uint64_t));
          return len; 
       }
    );
 
    intrinsics::set_intrinsic<intrinsics::action_data_size>(
       []() {
-         return (uint32_t)2*sizeof(eosio::name);
+         return (uint32_t)3*sizeof(eosio::name);
       }
    );
 
@@ -157,14 +190,23 @@ EOSIO_TEST_BEGIN(ncomplexsingle_test)
    apply("test"_n.value, "test"_n.value, "insert"_n.value);
 
    testnum = 3;
+   primary_key = 3;
+   apply("test"_n.value, "test"_n.value, "insert"_n.value);
+
+   primary_key = 2;
+   apply("test"_n.value, "test"_n.value, "get"_n.value);
+
+   testnum = 3;
    primary_key = 1;
    apply("test"_n.value, "test"_n.value, "modify"_n.value);
 
    primary_key = 1;
-   apply("test"_n.value, "test"_n.value, "move2"_n.value);
+   apply("test"_n.value, "test"_n.value, "move"_n.value);
+
    testnum = 3;
    primary_key = 1;
-   apply("test"_n.value, "test"_n.value, "erase3"_n.value);
+   apply("test"_n.value, "test"_n.value, "erase"_n.value);
+
 #if 0
    primary_key = 1;
    apply("test"_n.value, "test"_n.value, "move"_n.value);
---
+++ tmp/ncomplexsecondary.cpp
@@ -0,0 +1,197 @@
+#include <eosio/eosio.hpp>
+#include <eosio/tester.hpp>
+
+using namespace eosio;
+using namespace eosio::native;
+
+int testnum = 0;
+int primary_key = 0;
+
+class [[eosio::contract]] ncomplexsingle : public contract {
+   public:
+      using contract::contract;
+
+      ncomplexsingle(name receiver, name code,  datastream<const char*> ds): contract(receiver, code, ds) {}
+
+      [[eosio::action]]
+         void insert(uint64_t pk, name user, uint64_t secondary) {
+            eosio::print_f("INSERT\n");
+            eosio::print_f("pk: % -- user: % -- secondary %\n", pk, user, secondary);
+            address_index addresses( get_self(), get_first_receiver().value );
+            auto itr = addresses.find(pk);
+            check(itr == addresses.end(), "This record already exists");
+            addresses.emplace(get_self(), [&]( auto& row ) {
+               row.key = pk;
+               row.user = user;
+               row.secondary = secondary;
+            });
+         }
+
+   public:
+#if 0
+      [[eosio::action]]
+         void modify(uint64_t pk, name user) {
+            eosio::print_f("MODIFY\n");
+            address_index addresses( get_self(), get_first_receiver().value );
+            auto itr = addresses.find(pk);
+            check(itr != addresses.end(), "This record does not exist");
+            addresses.modify(itr, get_self(), [&]( auto& row ) {
+               row.user = user;
+            });
+         }
+
+      [[eosio::action]]
+         void erase(uint64_t pk) {
+            eosio::print_f("ERASE\n");
+            address_index addresses( get_self(), get_first_receiver().value);
+
+            auto itr = addresses.find(pk);
+            check(itr != addresses.end(), "Record does not exist but should");
+            addresses.erase(itr);
+            itr = addresses.find(pk);
+            check(itr == addresses.end(), "Record exists when it shouldn't");
+         }
+
+      [[eosio::action]]
+         void move(uint64_t pk) {
+            eosio::print_f("MOVE\n");
+            address_index addresses( get_self(), get_first_receiver().value);
+
+            auto itr = addresses.begin();
+            itr++;
+            itr++;
+            itr--;
+            itr--;
+            itr--;
+         }
+
+      [[eosio::action]]
+         void move2(uint64_t pk) {
+            eosio::print_f("MOVE2\n");
+            address_index addresses( get_self(), get_first_receiver().value);
+
+            auto& obj = addresses.get(1);
+            auto itr = addresses.iterator_to(obj);
+         }
+
+      [[eosio::action]]
+         void erase2(uint64_t pk) {
+            eosio::print_f("ERASE2\n");
+            address_index addresses( get_self(), get_first_receiver().value);
+
+            auto itr = addresses.end();
+            itr--;
+            addresses.erase(itr);
+         }
+
+      [[eosio::action]]
+         void erase3(uint64_t pk) {
+            eosio::print_f("ERASE3\n");
+            address_index addresses( get_self(), get_first_receiver().value);
+
+            auto itr = addresses.begin();
+            itr++;
+            itr++;
+            addresses.erase(itr);
+         }
+#endif
+
+   private:
+      struct [[eosio::table]] person {
+         uint64_t key;
+         name user;
+         uint64_t secondary;
+         uint64_t primary_key() const { return key; }
+         uint64_t by_secondary() const { return secondary; }
+      };
+      typedef eosio::multi_index<"people"_n, person, indexed_by<
+         name("secondary"), const_mem_fun<person, uint64_t, &person::by_secondary>
+      >> address_index;
+
+};
+
+EOSIO_DISPATCH(ncomplexsingle, (insert))
+
+EOSIO_TEST_BEGIN(ncomplexsingle_test)
+   intrinsics::set_intrinsic<intrinsics::read_action_data>(
+      [](void* m, uint32_t len) {
+         check(len <= 3*sizeof(eosio::name), "failed from read_action_data");
+         uint64_t pk = primary_key;
+         uint64_t user;
+         uint64_t secondary = (-primary_key) + 10;
+
+         switch (testnum) {
+         case 0:
+            user = eosio::name("bob").value;
+            break;
+         case 1:
+            user = eosio::name("alice").value;
+            break;
+         case 2:
+            user = eosio::name("john").value;
+            break;
+         case 3:
+            user = eosio::name("lucy").value;
+            break;
+         }
+
+         memcpy(m, &pk, sizeof(uint64_t));
+         memcpy((char*)m + sizeof(eosio::name), &user, sizeof(eosio::name));
+         memcpy((char*)m + 2*sizeof(uint64_t), &secondary, sizeof(uint64_t));
+         return len; 
+      }
+   );
+
+   intrinsics::set_intrinsic<intrinsics::action_data_size>(
+      []() {
+         return (uint32_t)3*sizeof(eosio::name);
+      }
+   );
+
+   intrinsics::set_intrinsic<intrinsics::current_receiver>(
+      []() {
+         return eosio::name{"test"}.value;
+      }
+   );
+
+
+   testnum = 0;
+   primary_key = 0;
+   apply("test"_n.value, "test"_n.value, "insert"_n.value);
+
+   testnum = 1;
+   primary_key = 1;
+   apply("test"_n.value, "test"_n.value, "insert"_n.value);
+
+   testnum = 2;
+   primary_key = 2;
+   apply("test"_n.value, "test"_n.value, "insert"_n.value);
+
+#if 0
+   testnum = 3;
+   primary_key = 1;
+   apply("test"_n.value, "test"_n.value, "modify"_n.value);
+
+   primary_key = 1;
+   apply("test"_n.value, "test"_n.value, "move2"_n.value);
+   testnum = 3;
+   primary_key = 1;
+   apply("test"_n.value, "test"_n.value, "erase3"_n.value);
+   primary_key = 1;
+   apply("test"_n.value, "test"_n.value, "move"_n.value);
+
+   primary_key = 1;
+   apply("test"_n.value, "test"_n.value, "erase"_n.value);
+
+   testnum = 3;
+   primary_key = 1;
+   apply("test"_n.value, "test"_n.value, "erase2"_n.value);
+#endif
+
+EOSIO_TEST_END
+
+int main(int argc, char** argv) {
+   silence_output(false);
+   EOSIO_TEST(ncomplexsingle_test);
+   return has_failed();
+}
